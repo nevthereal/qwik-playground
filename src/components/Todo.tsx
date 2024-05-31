79827bdeeb9ef55c4a315483bda1fc7a40428c0a
Neville Brem <bremneville@gmail.com>
2024-05-31 07:15:30 +0200

    added complete logic

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,16 +1,23 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 
-interface Todo {
+export interface TodoInterface {
   completed: boolean;
   content: string;
 }
 
-export default component$<Todo>(({ content }) => {
+export default component$<TodoInterface>(({ content, completed }) => {
+  const compStore = useSignal(completed);
+
   return (
     <div class="flex items-center justify-between rounded-2xl border border-slate-300 bg-slate-200 p-4">
       <p>{content}</p>
       <div class="flex gap-2">
-        <button class="rounded-2xl bg-green-300 p-2 font-bold">Complete</button>
+        <button
+          onClick$={() => (compStore.value = !compStore.value)}
+          class="rounded-2xl bg-green-300 p-2 font-bold"
+        >
+          {!compStore.value ? "Complete" : "Uncomplete"}
+        </button>
         <button class="rounded-2xl bg-red-300 p-2 font-bold">Delete</button>
       </div>
     </div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,29 @@
 import { component$ } from "@builder.io/qwik";
-import Todo from "~/components/Todo";
+import Todo, { type TodoInterface } from "~/components/Todo";
+
+const todos: TodoInterface[] = [
+  {
+    completed: false,
+    content: "Do the dishes",
+  },
+  {
+    completed: false,
+    content: "Learn Qwik",
+  },
+  {
+    completed: true,
+    content: "Make some noise",
+  },
+];
 
 export default component$(() => {
   return (
     <>
       <h1 class="mb-4 text-3xl font-bold">Todo App</h1>
       <div class="mx-auto flex w-[50vw] flex-col gap-4">
-        <Todo content="Do the dishes" completed={false} />
-        <Todo content="Do the dishes" completed={false} />
-        <Todo content="Do the dishes" completed={false} />
+        {todos.map(({ content, completed }) => {
+          return <Todo key={content} content={content} completed={completed} />;
+        })}
       </div>
     </>
   );
